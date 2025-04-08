1a38179ed71dc2e980dbda62a49ee5322cefa608
kulmanferdi <ferdi.kulman@gmail.com>
2025-04-08 16:52:49 +0200

    clean code

--- weather-api-app/WeatherInfo.cs
+++ weather-api-app/WeatherInfo.cs
@@ -6,12 +6,12 @@ public class WeatherInfo(JsonElement current)
 {
     private string ObservationTime { get; } = current.GetProperty("observation_time").GetString()!;
     private int Temperature { get; } = current.GetProperty("temperature").GetInt32();
-    private int RealFeel { get; } =  current.GetProperty("feelslike").GetInt32();
+    private int RealFeel { get; } = current.GetProperty("feelslike").GetInt32();
     private string [] WeatherDescriptions { get; } = current.GetProperty("weather_descriptions").EnumerateArray().Select(e => e.GetString()).ToArray()!;
-    private int WindSpeed { get; }= current.GetProperty("wind_speed").GetInt32();
+    private int WindSpeed { get; } = current.GetProperty("wind_speed").GetInt32();
     private string WindDirection { get; } = current.GetProperty("wind_dir").GetString()!;
     private int Humidity { get; } = current.GetProperty("humidity").GetInt32();
-    private int Uvi { get; } =  current.GetProperty("uv_index").GetInt32();
+    private int Uvi { get; } = current.GetProperty("uv_index").GetInt32();
     private int Visibility { get; } = current.GetProperty("visibility").GetInt32();
     
     public void Print()
