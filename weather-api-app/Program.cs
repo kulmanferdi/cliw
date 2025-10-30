bb5fdd53278b087fdc6612e277698d5a1836bfb1
kulmanferdi <ferdi.kulman@gmail.com>
2025-10-30 15:34:19 +0100

    rev

--- weather-api-app/Program.cs
+++ weather-api-app/Program.cs
@@ -2,7 +2,6 @@
 
 internal abstract class Program
 {
-    private const bool Razor = false;
     private static async Task Main()
     {
         //testing build pipeline
@@ -30,27 +29,18 @@ internal abstract class Program
                 Log.Information("Location set");
                 var (locationInfo, weatherInfo, astroInfo) = await service.GetCurrentWeatherAsync(location);
                 //var forecast = await service.GetTomorrowForecastAsync(location);
-
-                if (!Razor)
-                {
-                    Log.Information("Creating report...");
                 
-                    Console.Clear();
-                
-                    Console.WriteLine("CLIW report\n");
-                
-                    Console.WriteLine("Your location:");
-                    locationInfo?.Display();
-                    weatherInfo?.Display();
-                    astroInfo?.Display();
-                    //forecast?.Display();
-                }
-                else
-                {
-                    Console.Clear();
-                    await AppHost.RunAsync<Display>();
-                }
-                
+                Log.Information("Creating report...");
+            
+                Console.Clear();
+            
+                Console.WriteLine("CLIW report\n");
+            
+                Console.WriteLine("Your location:");
+                locationInfo?.Display();
+                weatherInfo?.Display();
+                astroInfo?.Display();
+                //forecast?.Display();
             }
             Log.Information("Weather report successful.");
             var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
